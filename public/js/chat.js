f6bfdd179261070f5ff2beafd66a8cb12f913fe0
Leslie Wen <leslie.wen@gmail.com>
2021-09-13 09:45:19 -0400

    send receive message between two windows

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket=io()
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e)=>{
+    e.preventDefault() //prevent page reload
+    const message=e.target.elements.message.value
+    socket.emit('sendMessage',message)//send from client to server
+})
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const path = require('path')
+const path=require('path')
 const http=require('http')
 const express=require('express')
 const socketio=require('socket.io')
-
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server) //server now supports websockets
-
 const port=process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
-
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
-    console.log("new websocket connection")//server setup and client connected to it
+io.on('connection',(socket)=>{
+    console.log("new websocket connection") //initial message
+    socket.emit('message', 'Welcome!')
+    socket.on('sendMessage',(message)=>{ //receive message from client
+        io.emit('message',message) //emit to efveryone
+    })
 })
 
 server.listen(port,()=>{
