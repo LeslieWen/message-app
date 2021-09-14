ca9a52699340b80bf1ab8a9f69bc121af0f6a925
Leslie Wen <leslie.wen@gmail.com>
2021-09-14 08:09:02 -0400

    added acknowledgement and profanity filter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path=require('path')
 const http=require('http')
 const express=require('express')
 const socketio=require('socket.io')
+const Filter=require('bad-words')
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server) //server now supports websockets
@@ -10,13 +11,21 @@ const publicDirectoryPath=path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket)=>{
-    console.log("new websocket connection") //initial message
+    console.log("New websocket connection")
     socket.emit('message', 'Welcome!')
-    socket.on('sendMessage',(message)=>{ //receive message from client
-        io.emit('message',message) //emit to efveryone
+    socket.broadcast.emit('message','1 user has joined.') 
+    socket.on('sendMessage',(message,callback)=>{ //receive sendMessage event from client
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callback('No bad words allowed.')
+        }
+        io.emit('message',message) //emit to everyone
+        callback() // acknowledge
+    })
+    socket.on('disconnect',()=>{
+        io.emit('message','1 user has left')
     })
 })
-
 server.listen(port,()=>{
     console.log(`Server is up on port ${port}!`)
 })
