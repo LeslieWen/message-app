ba885dc7db52a3aef782ef5c50fd3d93c6c4dd10
Leslie Wen <leslie.wen@gmail.com>
2021-09-18 21:31:11 -0400

    0.00.06

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,1 +1,31 @@
 const users=[]
+
+const addUser=({id, username,room})=>{
+    //Data Cleansing
+    username=username.trim().toLowerCase()
+    room=room.trim().toLowerCase()
+    if(!username){
+        return {
+            error:'A username is required'
+        }
+    }
+    const existingUsername=users.find((user)=>{
+        return user.room===room && username.name===username
+    })
+    if(existingUsername){
+        return{
+            error:'Entered username is not unique.'
+        }
+    }
+    //Populate array
+    const user={id,username,room}
+    users.push(user)
+    return{user}
+}
+const removeUser=(id)=>{
+    const index=users.findIndex((user)=>user.id===id)
+    if(index!==-1){
+        return users.splice(index,1)[0]
+    }
+}
+
